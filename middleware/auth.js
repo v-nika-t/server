e8b80a8f88757fde00eb70f4397814b04d541627
Tsishalovich Veranika <vt3199662@gmail.com>
2022-04-21 13:21:28 +0300

    Correct

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,8 +3,8 @@ const service = require('../services/auth.service');
 const auth =  (req, res, next) =>{
   service.verify(req, res)
          .then(data => {
-           res.status(200).json(data)
-           next()
+           if(data == 'User not found') {next()}
+           else {res.status(200).json(data)}
           }) 
 };
 
